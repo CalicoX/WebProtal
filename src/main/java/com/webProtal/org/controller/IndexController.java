bc7041acdbaedb321b9e3f983734997ba36a601e
CalicoX <pyh1234576@gmail.com>
2016-02-02 03:02:05 +0800

    Revert "123"

--- src/main/java/com/webProtal/org/controller/IndexController.java
+++ src/main/java/com/webProtal/org/controller/IndexController.java
@@ -3,19 +3,17 @@ package com.webProtal.org.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 
 @Controller
+@RequestMapping("/")
 public class IndexController {
 	/**
 	 * 首页
 	 * @param model
 	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@RequestMapping("/index")
 	public String index(Model model){
-		model.addAttribute("name", "测试");
-		System.out.println("init");
 		return "/showMessage";
 	}
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -2,7 +2,7 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
 <!DOCTYPE html>
-<html lang="zh-cn">
+<html lang="en">
 <head>
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
